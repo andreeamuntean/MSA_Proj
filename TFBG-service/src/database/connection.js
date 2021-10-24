c28b86c264708db83400eff0b76444ea773bdb66
victordaj <victordaj7@gmail.com>
2021-10-24 22:43:31 +0300

    <feature>[TFBG] - add backend project structure

---
+++ TFBG-service/src/database/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const CONSTANTS = require("../utils/constants");
+
+module.exports = () =>
+  mongoose.connect(CONSTANTS.DATABASE.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
---
+++ TFBG-service/src/utils/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: 9000,
+    DATABASE: {
+        URL: 'mongodb://localhost/TFBG'
+    }
+}
