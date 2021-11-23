576e5e9a04c16a191ff2b567b59d50d017297719
Anda <muntean.anda2000@gmail.com>
2021-11-23 16:49:11 +0200

    created first page schema

---
+++ TFBG-service/frontend/login.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "Login" >
+        <
+        header className = "Login-header" >
+
+        <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. <
+        /p> <
+        a className = "login-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
